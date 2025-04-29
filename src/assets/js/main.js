5ca3b159b8c5976c1e2e0fa13a61e7e2e1399854
Miguel Angel Cruz Gangoitia <miguelangelcruz@MacBook-Pro-de-Miguel.local>
2025-04-29 20:13:20 +0200

    update project

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -4,8 +4,8 @@ var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.
 
 
 // toggle menu mobile
-const menu = document.querySelector('.header_nav');
-const btnMenu = document.querySelector('.btn_nav');
+const menu = document.querySelector('.header_nav')
+const btnMenu = document.querySelector('.btn_nav')
 
 btnMenu.addEventListener('click', (e) => {
   e.preventDefault()
@@ -16,5 +16,29 @@ btnMenu.addEventListener('click', (e) => {
   } else  {
     menu.classList.remove('header_nav--open')
     btnMenu.classList.remove('btn_nav--open')
+  }
+})
+
+// open/hide filters mobile & tablet
+const divFilters = document.querySelector('.adventure_filters-content')
+const btnFilters = document.querySelector('.btn_filters')
+const btnCloseFilters = document.querySelector('#btnCloseFilters')
+const body = document.querySelector('body')
+
+btnFilters.addEventListener('click', (e) => {
+  e.preventDefault()
+
+  if (window.getComputedStyle(divFilters).display === 'none') {
+    divFilters.classList.add('adventure_filters-content--open')
+    body.classList.add('body--open')
+  }
+})
+
+btnCloseFilters.addEventListener('click', (e) => {
+  e.preventDefault()
+
+  if (window.getComputedStyle(divFilters).display === 'block') {
+    divFilters.classList.remove('adventure_filters-content--open')
+    body.classList.remove('body--open')
   }
 })
